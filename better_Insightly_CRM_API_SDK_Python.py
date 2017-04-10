d5e4abdd8e527fede7f1cbbd20a5dd50cc275027
mandau <mandau@users.noreply.github.com>
2017-04-10 10:18:46 -0700

    Update better_Insightly_CRM_API_SDK_Python.py

--- better_Insightly_CRM_API_SDK_Python.py
+++ better_Insightly_CRM_API_SDK_Python.py
@@ -5,7 +5,7 @@ import json
 from pprint import pprint
 
 #e.g. Contact data request
-j = requests.get('requests.get('https://api.insight.ly/v2.2/contacts/100000000', auth=HTTPBasicAuth 'Insightly API key goes here', ''))
+j = requests.get('https://api.insight.ly/v2.2/contacts/100000000', auth=HTTPBasicAuth('Insightly API key goes here', ''))
 d = json.loads(j.text)
 #get contact info ID
 pprint(d['CONTACTINFOS'][0]['CONTACT_INFO_ID'])
